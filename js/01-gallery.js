9275a5f7e1830c0f24dba471d7ca8f50a0426c2a
AleksandrMarkitan <0688138@gmail.com>
2022-08-29 23:49:56 +0300

    Виконав обидві частини ДЗ

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,12 +1,7 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
-console.log(galleryItems);
 
 const refs = {
   gallery: document.querySelector('.gallery'),
-  head: document.querySelector('head'),
-  body: document.querySelector('body'),
-  //   basicLightbox: document.querySelector('basicLightbox'),
 };
 
 const markup = galleryItems.reduce(
@@ -14,20 +9,7 @@ const markup = galleryItems.reduce(
     (acc += `<div class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/></a></div>`),
   ``
 );
-
 refs.gallery.insertAdjacentHTML('beforeend', markup);
-// refs.head.insertAdjacentHTML(
-//   'beforeend',
-//   `<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/basiclightbox@5.0.4/dist/basicLightbox.min.css"/>`
-// );
-// refs.gallery.insertAdjacentHTML(
-//   'afterend',
-//   `<script src="https://cdn.jsdelivr.net/npm/basiclightbox@5.0.4/dist/basicLightbox.min.js"></script>`
-// );
-
-console.log(refs.gallery);
-console.log(refs.head);
-console.log(refs.body);
 
 refs.gallery.addEventListener('click', onItemHandler);
 
@@ -46,7 +28,6 @@ function onItemHandler(e) {
         pictureInModal.close();
         document.removeEventListener('keydown', closeModal);
       }
-      console.log('code: ', e.code);
     }
   }
 }
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const refs = {
+  gallery: document.querySelector('.gallery'),
+};
+
+const markup = galleryItems.reduce(
+  (acc, { preview, original, description }) =>
+    (acc += `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`),
+  ``
+);
+refs.gallery.insertAdjacentHTML('beforeend', markup);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionType: 'attr',
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
